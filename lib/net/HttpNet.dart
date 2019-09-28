746385038878069432e42754f05484233f6855de
Dector <chenkai851206>
2019-09-28 21:26:43 +0800

    优化项目

--- lib/net/HttpNet.dart
+++ lib/net/HttpNet.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:collection';
 import 'dart:convert';
 
@@ -13,7 +12,7 @@ class HttpNet {
   // 工厂模式
   factory HttpNet() => _getInstance();
 
-  Dio dio;
+  Dio _dio;
 
   static HttpNet get instance => _getInstance();
   static HttpNet _instance;
@@ -31,14 +30,13 @@ class HttpNet {
   }
 
   _initDio() {
-    CancelToken token = new CancelToken();
-    dio = new Dio();
-    dio.options.connectTimeout = 15 * 1000;
-    dio.options.receiveTimeout = 15 * 1000;
-    dio.options.baseUrl = ApiUtils.baseUrl;
-    dio.options.responseType = ResponseType.json;
+    _dio = new Dio();
+    _dio.options.connectTimeout = 15 * 1000;
+    _dio.options.receiveTimeout = 15 * 1000;
+    _dio.options.baseUrl = ApiUtils.baseUrl;
+    _dio.options.responseType = ResponseType.json;
     if (Utils.isDebug)
-      dio.interceptors.add(LogInterceptor(responseBody: true)); //开启请求日志
+      _dio.interceptors.add(LogInterceptor(responseBody: Utils.isDebug)); //开启请求日志
   }
 
   request(MethodTypes methodTypes, String path, Function(String) success,
@@ -46,32 +44,44 @@ class HttpNet {
       {HashMap<String, dynamic> params,
       Function(String) errorCallback,
       dynamic data,
-      HashMap<String, dynamic> headers}) {
+      HashMap<String, dynamic> headers}) async {
+    CancelToken token = new CancelToken();
     if (headers == null) headers = new HashMap();
-//    headers['Cookie'] = "${ApiUtils.cookieKey}=${ApiUtils.cookieValue}";
+    headers['Cookie'] = "${ApiUtils.cookieKey}=${ApiUtils.cookieValue}";
     var options = new Options(headers: headers);
 
-    Future<Response<String>> sValue;
+    Response<String> sValue;
     if (methodTypes == MethodTypes.GET) {
-      sValue = dio.get(path, queryParameters: params, options: options);
+      sValue = await _dio.get(path,
+          queryParameters: params, options: options, cancelToken: token);
     } else if (methodTypes == MethodTypes.POST) {
-      sValue =
-          dio.post(path, queryParameters: params, options: options, data: data);
+      sValue = await _dio.post(path,
+          queryParameters: params,
+          options: options,
+          data: data,
+          cancelToken: token);
     } else if (methodTypes == MethodTypes.PUT) {
-      sValue =
-          dio.put(path, queryParameters: params, options: options, data: data);
+      sValue = await _dio.put(path,
+          queryParameters: params,
+          options: options,
+          data: data,
+          cancelToken: token);
     } else if (methodTypes == MethodTypes.DELETE) {
-      sValue = dio.delete(path,
-          queryParameters: params, options: options, data: data);
+      sValue = await _dio.delete(path,
+          queryParameters: params,
+          options: options,
+          data: data,
+          cancelToken: token);
     }
 
-    sValue.then((value) {
+    try {
       if (path != ApiUtils.post_upload_img && path != ApiUtils.post_sendsms) {
-        BaseModel model = BaseModel.fromJson(jsonDecode(value.data));
-        if (value != null && isNotEmpty(value.data)) {
+        BaseModel model = BaseModel.fromJson(jsonDecode(sValue.data));
+        if (sValue != null && isNotEmpty(sValue.data)) {
           if (model.status == 200) {
-            success(value.data);
+            success(sValue.data);
           } else if (model.status == 302) {
+            token.cancel();
             relogin();
           } else {
             Utils.showToast(model.msg);
@@ -80,23 +90,49 @@ class HttpNet {
           errorCallback("");
         }
       } else {
-        success(value.data);
+        success(sValue.data);
       }
-    }).catchError((error) {
-      Utils.logs("错误 = ${error.toString()}");
-      if (null != error) {
-        bool dioError = error is DioError;
-        if (dioError) {
-          var errorStr = error as DioError;
-          if (error == DioErrorType.CONNECT_TIMEOUT) {
-            Utils.showToast("网络连接超时，请重试");
-          } else {
-            Utils.showToast(errorStr.response.data);
-            errorCallback(errorStr.response.data);
-          }
-        }
+    } on DioError catch (error) {
+      if (error.type == DioErrorType.CONNECT_TIMEOUT) {
+        Utils.showToast("网络连接超时，请重试");
+      } else {
+        Utils.showToast(error.response.data);
+        errorCallback(error.response.data);
       }
-    });
-  }
+    }
 
+//    sValue.then((value) {
+//      if (path != ApiUtils.post_upload_img && path != ApiUtils.post_sendsms) {
+//        BaseModel model = BaseModel.fromJson(jsonDecode(value.data));
+//        if (value != null && isNotEmpty(value.data)) {
+//          if (model.status == 200) {
+//            success(value.data);
+//          } else if (model.status == 302) {
+//            token.cancel();
+//            relogin();
+//          } else {
+//            Utils.showToast(model.msg);
+//          }
+//        } else {
+//          errorCallback("");
+//        }
+//      } else {
+//        success(value.data);
+//      }
+//    }).catchError((error) {
+//      Utils.logs("错误 = ${error.toString()}");
+//      if (null != error) {
+//        bool dioError = error is DioError;
+//        if (dioError) {
+//          var errorStr = error as DioError;
+//          if (error == DioErrorType.CONNECT_TIMEOUT) {
+//            Utils.showToast("网络连接超时，请重试");
+//          } else {
+//            Utils.showToast(errorStr.response.data);
+//            errorCallback(errorStr.response.data);
+//          }
+//        }
+//      }
+//    });
+  }
 }
